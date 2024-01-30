b1006ad0f50427aa66565af3b228fc8a9b755932
Deni Supriyatna <denisupriyatna01@gmail.com>
2024-01-30 21:08:46 +0700

    initial commit

---
+++ tes-1.js
@@ -0,0 +1,11 @@
+const text = "Belajar React bareng WPU";
+
+var app = document.getElementById("app");
+
+let h1 = document.createElement("h1");
+
+h1.append(text);
+
+app.append(h1);
+
+console.log("asu");
